a0a7520e7464d0d79723d7217d51d3d88ae9c592
davilopess <davi.lopes.amorim2014@hotmail.com>
2020-11-04 20:17:25 -0300

    Listagem de usuario

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,29 @@
+<?php 
+namespace estoque\Http\Controllers;
+
+use estoque\Produto;
+use Illuminate\Support\Facades\DB;
+use Request;
+
+class ProdutoController extends Controller {
+    public function lista(){
+
+        $produtos = DB::select('select * from produtos');
+
+        return view('produto.listagem', ['produtos' => $produtos]);
+    }
+
+    public function mostra($id){
+        
+        $resposta = DB::select('select * from produtos where id = ?', [$id]);
+        
+        
+        
+        if(empty($resposta)) {
+            return "Esse produto não existe";
+        }
+        
+        return view('produto.detalhes')->with('p', $resposta[0]);
+
+    }
+}
